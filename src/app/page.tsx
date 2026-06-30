eebe9ab8a7554c3d39154bd014a124c09e141593
Alini Canedo <alini.canedo@gmail.com>
2026-06-30 17:24:20 -0300

    feat: add global app store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
+'use client';
+
 import { APP_NAME } from '@/utils/constants';
-import { ENDPOINTS } from '@/services';
+import { useAppStore } from '@/stores/app.store';
 
 export default function Home() {
-  console.log(ENDPOINTS);
+  const { isLoading, setLoading } = useAppStore();
 
   return (
     <main>
       <h1>{APP_NAME}</h1>
+
+      <p>
+        Loading:
+        {' '}
+        {String(isLoading)}
+      </p>
+
+      <button onClick={() => setLoading(!isLoading)}>
+        Alternar Loading
+      </button>
     </main>
   );
 }
