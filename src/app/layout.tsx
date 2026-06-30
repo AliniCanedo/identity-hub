5a7a10eb5f6af633481e844bd80a6113dbb88d99
Alini Canedo <alini.canedo@gmail.com>
2026-06-30 17:20:22 -0300

    feat: configure TanStack Query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { QueryProvider } from '@/providers/QueryProvider';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +29,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <QueryProvider>
+        <body className="min-h-full flex flex-col">{children}</body>
+      </QueryProvider>
     </html>
   );
 }
