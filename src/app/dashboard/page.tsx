96c387b303bb92d523d6a687ad8de7b334e892a3
Alini Canedo <alini.canedo@gmail.com>
2026-07-04 09:13:55 -0300

    feat(router): add application routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { AppLayout } from '@/components/layout/AppLayout/AppLayout';
+
+export default function DashboardPage() {
+  return (
+    <AppLayout>
+      <h2>Dashboard</h2>
+
+      <p>Bem-vinda ao Identity Hub.</p>
+    </AppLayout>
+  );
+}
